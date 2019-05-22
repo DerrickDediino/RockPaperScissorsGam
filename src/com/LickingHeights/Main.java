ce3ff5ba3a6c0d04412842c0772f1eda65d0731b
Derrick <Derrick.Dedino.2022@student.lhschools.org>
2019-05-22 13:46:09 -0400

    Introduce Game.

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -4,5 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        System.out.println("Welcome to the Rock, Paper, Scissors Game.  You have 7 rounds.");
+        System.out.println("Pick Rock, Paper, or Scissors to Start (Rock = \"0\", Paper = \"1\", and Scissors = \"2\").");
     }
 }
